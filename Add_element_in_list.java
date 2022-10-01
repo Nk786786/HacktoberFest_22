77ac7c895d562a0234c73fdbd7157cee84f070fd
Dev-Wizard <72193266+Dev-Wizard@users.noreply.github.com>
2022-10-01 23:58:29 +0530

    Create Add_element_in_list.java

---
+++ Add_element_in_list.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.ArrayList;
+ 
+class GFG {
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+ 
+        // add method for integer ArrayList
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
+    }
+}
